c6438a34c1c1a480df801235f029cca35f7287f3
Ihar Anikeyenka <ihar.anikeyenka@gmail.com>
2024-04-05 16:37:18 +0200

    guard and decorators

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,30 @@
-import { Module } from '@nestjs/common';
-import { UserService } from './user.service';
-import { UserController } from './user.controller';
+import { Module } from '@nestjs/common'
+import { UserService } from './user.service'
+import { UserController } from './user.controller'
+import { TypegooseModule } from 'nestjs-typegoose'
+import { UserModel } from './user.model'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { JwtModule } from '@nestjs/jwt'
+import { getJWTConfig } from '../config/jwt.config'
 
 @Module({
+  imports: [
+    TypegooseModule.forFeature([
+      {
+        typegooseClass: UserModel,
+        schemaOptions: {
+          collection: 'User',
+        },
+      },
+    ]),
+    ConfigModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getJWTConfig,
+    }),
+  ],
   providers: [UserService],
-  controllers: [UserController]
+  controllers: [UserController],
 })
 export class UserModule {}
