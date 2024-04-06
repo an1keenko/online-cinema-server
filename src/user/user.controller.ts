de4da979c9fd614f2461c3d866a744958fe2d22c
Ihar Anikeyenka <ihar.anikeyenka@gmail.com>
2024-04-06 15:12:37 +0200

    user actions

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,18 @@
-import { Controller, Get } from '@nestjs/common'
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Param,
+  Put,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common'
 import { UserService } from './user.service'
 import { Auth } from '../auth/decorators/auth.decorators'
 import { User } from './decorators/user.decorator'
+import { UpdateUserDto } from './dto/update-user.dto'
+import { IdValidationPipe } from '../pipes/id.validation.pipe'
 
 @Controller('users')
 export class UserController {
@@ -11,5 +22,24 @@ export class UserController {
   @Auth()
   async getProfile(@User('_id') _id: string) {
     return this.userService.byId(_id)
+  }
+
+  @UsePipes(new ValidationPipe())
+  @Put('profile')
+  @HttpCode(200)
+  @Auth()
+  async updateProfile(@User('_id') _id: string, @Body() dto: UpdateUserDto) {
+    return this.userService.updateProfile(_id, dto)
+  }
+
+  @UsePipes(new ValidationPipe())
+  @Put(':id')
+  @HttpCode(200)
+  @Auth('admin')
+  async updateUser(
+    @Param('id', IdValidationPipe) id: string,
+    @Body() dto: UpdateUserDto,
+  ) {
+    return this.userService.updateProfile(id, dto)
   }
 }
